4e2820bd8cbab2275822655a5b29d625b7712a2f
priscila-182 <111939869+priscila-182@users.noreply.github.com>
2022-09-26 21:41:44 -0300

    actualización 1.3

--- app/src/main/java/com/example/cbs_appv10/PaginaPrincipal_Activity.java
+++ app/src/main/java/com/example/cbs_appv10/PaginaPrincipal_Activity.java
@@ -1,16 +1,32 @@
 package com.example.cbs_appv10;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 
 public class PaginaPrincipal_Activity extends AppCompatActivity {
 
+    RecyclerView featuredRecycler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_pagina_principal);
+
+        featuredRecycler = findViewById(R.id.featured_recycle);
+
+        featuredRecycler();
+
+    }
+
+    private void featuredRecycler() {
+
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
     }
 }
